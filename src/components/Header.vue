ed428a9fc7a1bae649ab6221b9662fabd66a9033
Jakob Hammer <Jakob@thomasminde.dk>
2020-11-05 11:16:43 +0100

    Commit

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,7 +3,7 @@
     <h1 class="border border-dark text-center">
       DR Music
     </h1>
-    <SearchBar @change-filter="$emit('change-filter', "Hallo")" />
+    <SearchBar @change-filter="$emit('change-filter', filters)" />
   </div>
 </template>
 
