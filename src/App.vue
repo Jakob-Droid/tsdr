e4e64df08f68c7c1d4b1b44446e0600c9e108bbb
Jakob Hammer <Jakob@thomasminde.dk>
2020-11-05 09:25:37 +0100

    Initial commit
    Co-authored-by: Kasper Stilling Andersen <kasp762s@edu.easj.dk>
    Co-authored-by: Magnus Marcus Hansen <magn4364@edu.easj.dk>

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,45 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div>
+    <div>
+      <Header v-on:change-filter="searched" />
+    </div>
+    <div>
+      <div class="container">
+        <RecordCard :records="records" />
+      </div>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
-import { Options, Vue } from 'vue-class-component';
-import HelloWorld from './components/HelloWorld.vue';
+import { Options, Vue } from "vue-class-component";
+import RecordCard from "./components/RecordCard.vue";
+import Header from "./components/Header.vue";
+import SearchBar from "./components/SearchBar.vue";
 
 @Options({
+  data: function() {
+    return {
+      records: Array,
+    };
+  },
   components: {
-    HelloWorld,
+    RecordCard,
+    Header,
+    SearchBar,
+  },
+  mounted: function() {
+    fetch("https://drmusicrestservice.azurewebsites.net/api/DRMusicRecord")
+      .then((response) => response.json())
+      .then((data) => {
+        this.records = data;
+      });
+  },
+  computed: {
+    searched: function(name: any) {
+      console.log("FUCK THIS SHIT IT FUCKING DOESN'T WORK!" + name[0]);
+    },
   },
 })
 export default class App extends Vue {}
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/SearchBar.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <select id="dropdown" @change="$emit('change-filter', filters)">
+      <option v-bind:Titel="filters" value="Title">Titel</option>
+      <option v-bind:Artist="filters">Artist</option>
+      <option v-bind:YearOfProduction="filters">Year of Production</option>
+      <option v-bind:Album="filters">Album</option>
+    </select>
+  </div>
+</template>
+<script>
+export default {
+  name: "SearchBar",
+  props: [],
+  data: function() {
+    return {
+      filters: "",
+      search: "",
+    };
+  },
+  method: function() {
+    return null;
+  },
+};
+</script>
+
+<style scoped>
+#dropdown {
+  max-width: 20vw;
+  align-content: center;
+}
+</style>
